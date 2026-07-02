5c7e14d8c988b5838a3bdd3a7de1aa23cc8f6423
Hafitz <71178188+breakdowns@users.noreply.github.com>
2026-07-02 08:13:17 +0700

    Update and rename coding.json to coding.js

---
+++ data/coding.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        username: "hafitz",
+        tag: "0001",
+        timestamp: "Baru saja",
+        title: "Dev Log: Sukses Kelarin Sistem SPA Blog Khas Discord! 🚀",
+        content: "Akhirnya selesai juga ngulik sistem pindah halaman instan tanpa harus bikin file HTML panjang-panjang. Sekarang kalau mau nambah artikel tinggal nulis di file JS dalam folder data aja.",
+        emoji: "💻",
+        reactions: 10
+    }
+];
