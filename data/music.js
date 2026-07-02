57ff0d43b069b6e2a9e726bcd88b55c2b8b07e75
Hafitz <71178188+breakdowns@users.noreply.github.com>
2026-07-02 08:33:25 +0700

    Update music post with new timestamp and content

--- data/music.js
+++ data/music.js
@@ -2,10 +2,10 @@ var dataMusic = [
     {
         username: "hafitz",
         tag: "0001",
-        timestamp: "Hari ini pukul 11:15",
-        title: "🎧 Now Playing on Spotify",
-        content: "Lagi dengerin beberapa track metalcore dan synthwave buat nemenin fokus coding hari ini. Link playlist lengkap udah gue sematin di welcome-links ya, langsung klik aja kalau mau ikut dengerin!",
-        emoji: "🎵",
-        reactions: 15
+        timestamp: "Hari ini pukul 08:32",
+        title: "🎵 Breakdowns Official Playlist is LIVE!",
+        content: "Buat lu pada yang nyari temen fokus pas lagi coding, nge-game, atau sekadar pengen nyantai sore, langsung gas dengerin playlist buatan gue di Spotify. Isinya udah gue racik biar dapet *vibes* yang pas dan konsisten sepanjang nge-track. <br><br>👉 <strong>Link Playlist:</strong> <a href='https://open.spotify.com/playlist/38xujwdY9rSGTDm3eqzbgL?si=Z_rrJ-_-RyuJqnFBFi3aGg' target='_blank' class='text-discord-blurple hover:underline font-medium'>Klik di sini untuk dengerin langsung</a>",
+        emoji: "🎧",
+        reactions: 18
     }
 ];
